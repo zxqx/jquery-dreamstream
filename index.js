fd59c97f4fa9df20cc7d91687b74fef6f42eeb44
Zak Angelle <zaknotzak@gmail.com>
2014-06-10 01:25:39 -0500

    Refactored some

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var $           = require('jquery');
 var DreamStream = require('./lib/DreamStream.js');
 
-// TEMP - expose global jQua
-global.$ = $;
-
 $.fn.dreamStream = function(options) {
   return this.each(function() {
     var dreamStream = new DreamStream($(this), options);
