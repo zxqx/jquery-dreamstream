2083601628b31489b2b639f1bc2f1b1ea00cbe5b
Zak Angelle <zaknotzak@gmail.com>
2014-06-10 01:53:21 -0500

    Removed old index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var $           = require('jquery');
-var DreamStream = require('./lib/DreamStream.js');
-
-$.fn.dreamStream = function(options) {
-  return this.each(function() {
-    var dreamStream = new DreamStream($(this), options);
-    dreamStream.start();
-  });
-};
