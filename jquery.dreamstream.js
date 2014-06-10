5a638bcae5b6d9053f9edabd280e790d78fc3967
Zak Angelle <zaknotzak@gmail.com>
2014-06-10 01:51:58 -0500

    UMDified

--- jquery.dreamstream.js
+++ jquery.dreamstream.js
@@ -1,6 +1,25 @@
-module.exports = DreamStream;
-
-var $ = require('jquery');
+/**
+ * UMDified jQuery plugin
+ */
+(function (factory) {
+if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery'], factory);
+} else if (typeof exports === 'object') {
+    // Node/CommonJS style for Browserify
+    module.exports = factory;
+} else {
+    // Browser globals
+    factory(jQuery);
+}
+}(function($) {
+  $.fn.dreamStream = function(options) {
+    return this.each(function() {
+      var dreamStream = new DreamStream($(this), options);
+      dreamStream.start();
+    });
+  };
+}));
 
 /**
  * @param {object} el jQuery wrapped DOM element
