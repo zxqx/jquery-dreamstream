695ceecac6c54a4e4d48c14efa12d2e7feac2f96
Zak Angelle <zaknotzak@gmail.com>
2014-06-12 12:08:41 -0500

    Better index.js

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 var DreamStream = require('./lib/DreamStream.js');
 
-/**
- * UMDified jQuery plugin
- */
-(function(factory) {
-if (typeof define === 'function' && define.amd) {
-  // AMD. Register as an anonymous module.
-  define(['jquery'], factory);
-} else if (typeof exports === 'object') {
-  // Node/CommonJS style for Browserify
-  module.exports = factory;
-} else {
-  // Browser globals
-  factory(jQuery);
-}
-}(function($) {
+module.exports = function($) {
   $.fn.dreamStream = function(options) {
     return this.each(function() {
       var dreamStream = new DreamStream($(this), options);
       dreamStream.start();
     });
   };
-}));
+};
